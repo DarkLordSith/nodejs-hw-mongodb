2e372d4442afd031e7826baab2c812b0b5224946
Vladimir Tryasunov <tryasunov933@gmail.com>
2025-02-28 12:19:14 +0200

    connect mongodb

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
-setupServer();
+async function startApp() {
+  await initMongoConnection();
+  setupServer();
+}
+
+startApp();
