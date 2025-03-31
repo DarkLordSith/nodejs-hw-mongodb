41d8ef67a256ff04052493968b712db189484d4a
Vladimir Tryasunov <tryasunov933@gmail.com>
2025-03-31 12:12:49 +0300

    hw6 fix

--- src/controllers/sendResetEmailController.js
+++ src/controllers/sendResetEmailController.js
@@ -25,7 +25,7 @@ export const sendResetEmailController = async (req, res) => {
 
     res.status(200).json({
       status: 200,
-      message: 'Reset password email has ben successfully sent.',
+      message: 'Reset password email has been successfully sent.',
       data: {},
     });
   } catch (error) {
